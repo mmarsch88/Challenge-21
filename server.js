b3f9df49d54a7734ab8423aa57541706e19dfc04
mmarsch88 <109492362+mmarsch88@users.noreply.github.com>
2022-12-07 21:46:58 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { ApolloServer} = require('apollo-server-express');
+const path = require('path');
+const { authMiddleware} = require('./utils/auth');
+const db = require('./config/connection');
+const { typeDefs, resolvers} = require('./schemas');
+
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+})
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({app});
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+}
+
+startApolloServer(typeDefs, resolvers);
+
